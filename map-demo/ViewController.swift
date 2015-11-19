b01143aa970cc358ee97070e1c9a5d7704c99dfb
jhars <jjharlan06@gmail.com>
2015-11-18 19:19:58 -0800

    Initial Commit

---
+++ map-demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  map-demo
+//
+//  Created by Jon Harlan on 11/18/15.
+//  Copyright © 2015 Jon Harlan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
