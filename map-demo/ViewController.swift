36186ee078d3250c7866d0ce3ea1124d17ecd368
jhars <jjharlan06@gmail.com>
2015-11-18 20:59:46 -0800

    my location firing

--- map-demo/ViewController.swift
+++ map-demo/ViewController.swift
@@ -7,19 +7,59 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController, CLLocationManagerDelegate {
+//    MARK: - Outlets
+    @IBOutlet weak var myMapView: MKMapView!
+//  MARK: Local Vaiables
+    var locationManager = CLLocationManager()
+// MARK: - LifeCycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        locationManager.delegate = self
+        locationManager.startUpdatingLocation()
+        locationManager.requestWhenInUseAuthorization()
+    }
+    // MARK: - Button interactions
+    @IBAction func findMeBtn(sender: AnyObject) {
+        locationManager.requestLocation()
+        print("find me button")
+        if let location = locationManager.location {
+            addPin(location)
+        }
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // MARK:  - CLLocationMangerDelegte Methods
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]){
+        
+        print("did this shit work?")
+        let lastLocation = locations[locations.count-1]
+        centerMapOnLocation(lastLocation)
     }
 
-
+    func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
+    
+    }
+    
+    // MARK: - Map Interaction Methods
+    let regionRadius: CLLocationDistance = 1000
+    func centerMapOnLocation(location: CLLocation) {
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate, regionRadius * 2.0, regionRadius * 2.0)
+        self.myMapView.setRegion(coordinateRegion, animated: true)
+    }
+    
+    
+    
+    func addPin(location:CLLocation) {
+        //create annotation
+        let annotation = MKPointAnnotation()
+        //set its properties: coords, name
+        annotation.title = "you are here"
+        annotation.coordinate = location.coordinate
+        myMapView.addAnnotation(annotation)
+    }
 }
-
